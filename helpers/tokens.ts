cbaba9d05b62e1e306ff33ab6c4bde3cb06fbb23
Taras Yevtushenko <taras.evt@gmail.com>
2022-04-07 10:33:58 +0300

    allow for more granular input precision

--- helpers/tokens.ts
+++ helpers/tokens.ts
@@ -4,10 +4,10 @@ import { Decimal } from 'decimal.js'
 
 /*
  * Should return a precision amount, no greater than 18 or the max precision
- * for that token, for which a single fractional unit is approximately equivalent to 0.01 USD.
+ * for that token, for which a single fractional unit is approximately equivalent to 0.001 USD.
  *
  * e.g:
- * 1 ETH === 10,000 USD :: Precision - 7 :: 0.0000001 ETH = 0.01 USD
+ * 1 ETH === 10,000 USD :: Precision - 8 :: 0.00000001 ETH = 0.001 USD
  */
 export function calculateTokenPrecisionByValue({
   token,
@@ -20,8 +20,9 @@ export function calculateTokenPrecisionByValue({
   const price = new Decimal(usdPrice.toString())
   const tokenPrecision = getToken(token).precision
   const tokenPricePerDollar = price.pow(-1)
-  const tokenPricePerCent = tokenPricePerDollar.times(new Decimal(10).pow(-2))
+  const tokenPricePerCent = tokenPricePerDollar.times(new Decimal(10).pow(-3))
 
   const magnitude = tokenPricePerCent.logarithm(ten).times(-1).floor()
+
   return magnitude.gt(tokenPrecision) ? tokenPrecision : magnitude.gt(0) ? magnitude.toNumber() : 0
 }

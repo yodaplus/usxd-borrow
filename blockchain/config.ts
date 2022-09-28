fda5229dac9ee62d4b2b5e18d68754fec71f5d95
Sudeep <49141455+rsud33p@users.noreply.github.com>
2022-09-28 08:37:04 +0530

    Disabled Mainnet borrow

--- blockchain/config.ts
+++ blockchain/config.ts
@@ -33,7 +33,7 @@ import { networkNameToId } from '@oasisdex/web3-context/lib/src/network'
 
 networkNameToId['apothem'] = 51
 networkNameToId['main'] = 51
-networkNameToId['mainnet'] = 50
+//networkNameToId['mainnet'] = 50
 
 export function contractDesc(abi: any, address: string): ContractDesc {
   return { abi, address }
@@ -96,46 +96,46 @@ const protoMain = {
 export type NetworkConfig = typeof protoMain
 
 const apothem: NetworkConfig = protoMain
-const mainnet: NetworkConfig = {
-  ...protoMain,
-  id: '50',
-  name: 'mainnet',
-  label: 'mainnet',
-  infuraUrl: 'https://rpc.xinfin.yodaplus.net',
-  collaterals: getCollaterals(mainnetddresses),
-  tokens: {
-    ...getCollateralTokens(mainnetddresses),
-    WETH: contractDesc(eth, mainnetddresses.ETH),
-    DAI: contractDesc(erc20, mainnetddresses.MCD_DAI),
-    USXD: contractDesc(erc20, mainnetddresses.MCD_DAI),
-  },
-  joins: {
-    ...getCollateralJoinContracts(mainnetddresses),
-  },
-  getCdps: contractDesc(getCdps, mainnetddresses.GET_CDPS),
-  mcdOsms: getOsms(mainnetddresses),
-  mcdPot: contractDesc(mcdPot, mainnetddresses.MCD_POT),
-  mcdJug: contractDesc(mcdJug, mainnetddresses.MCD_JUG),
-  mcdEnd: contractDesc(mcdEnd, mainnetddresses.MCD_END),
-  mcdSpot: contractDesc(mcdSpot, mainnetddresses.MCD_SPOT),
-  mcdDog: contractDesc(mcdDog, mainnetddresses.MCD_DOG),
-  dssCdpManager: contractDesc(dssCdpManager, mainnetddresses.CDP_MANAGER),
-  vat: contractDesc(vat, mainnetddresses.MCD_VAT),
-  mcdJoinDai: contractDesc(mcdJoinDai, mainnetddresses.MCD_JOIN_DAI),
-  dsProxyRegistry: contractDesc(dsProxyRegistry, mainnetddresses.PROXY_REGISTRY),
-  dsProxyFactory: contractDesc(dsProxyFactory, mainnetddresses.PROXY_FACTORY),
-  dssProxyActions: contractDesc(dssProxyActions, mainnetddresses.PROXY_ACTIONS),
-  fmm: mainnetddresses.MCD_FLASH,
-  etherscan: {
-    url: 'https://xdc.blocksscan.io',
-    apiUrl: '',
-    apiKey: '',
-  },
-  taxProxyRegistries: [mainnetddresses.PROXY_REGISTRY],
-  dssProxyActionsDsr: contractDesc(dssProxyActionsDsr, mainnetddresses.PROXY_ACTIONS_DSR),
-  cacheApi: 'https://borrow-cache.yodaplus.net/v1',
-  features: new Set<'multiply'>([]),
-}
+// const mainnet: NetworkConfig = {
+//   ...protoMain,
+//   id: '50',
+//   name: 'mainnet',
+//   label: 'mainnet',
+//   infuraUrl: 'https://rpc.xinfin.yodaplus.net',
+//   collaterals: getCollaterals(mainnetddresses),
+//   tokens: {
+//     ...getCollateralTokens(mainnetddresses),
+//     WETH: contractDesc(eth, mainnetddresses.ETH),
+//     DAI: contractDesc(erc20, mainnetddresses.MCD_DAI),
+//     USXD: contractDesc(erc20, mainnetddresses.MCD_DAI),
+//   },
+//   joins: {
+//     ...getCollateralJoinContracts(mainnetddresses),
+//   },
+//   getCdps: contractDesc(getCdps, mainnetddresses.GET_CDPS),
+//   mcdOsms: getOsms(mainnetddresses),
+//   mcdPot: contractDesc(mcdPot, mainnetddresses.MCD_POT),
+//   mcdJug: contractDesc(mcdJug, mainnetddresses.MCD_JUG),
+//   mcdEnd: contractDesc(mcdEnd, mainnetddresses.MCD_END),
+//   mcdSpot: contractDesc(mcdSpot, mainnetddresses.MCD_SPOT),
+//   mcdDog: contractDesc(mcdDog, mainnetddresses.MCD_DOG),
+//   dssCdpManager: contractDesc(dssCdpManager, mainnetddresses.CDP_MANAGER),
+//   vat: contractDesc(vat, mainnetddresses.MCD_VAT),
+//   mcdJoinDai: contractDesc(mcdJoinDai, mainnetddresses.MCD_JOIN_DAI),
+//   dsProxyRegistry: contractDesc(dsProxyRegistry, mainnetddresses.PROXY_REGISTRY),
+//   dsProxyFactory: contractDesc(dsProxyFactory, mainnetddresses.PROXY_FACTORY),
+//   dssProxyActions: contractDesc(dssProxyActions, mainnetddresses.PROXY_ACTIONS),
+//   fmm: mainnetddresses.MCD_FLASH,
+//   etherscan: {
+//     url: 'https://xdc.blocksscan.io',
+//     apiUrl: '',
+//     apiKey: '',
+//   },
+//   taxProxyRegistries: [mainnetddresses.PROXY_REGISTRY],
+//   dssProxyActionsDsr: contractDesc(dssProxyActionsDsr, mainnetddresses.PROXY_ACTIONS_DSR),
+//   cacheApi: 'https://borrow-cache.yodaplus.net/v1',
+//   features: new Set<'multiply'>([]),
+// }
 
 const hardhat: NetworkConfig = {
   ...protoMain,
@@ -157,8 +157,11 @@ const hardhat: NetworkConfig = {
   ),
 }
 
-export const networksById = keyBy([hardhat, apothem, mainnet], 'id')
-export const networksByName = keyBy([hardhat, apothem, mainnet], 'name')
+// export const networksById = keyBy([hardhat, apothem, mainnet], 'id')
+// export const networksByName = keyBy([hardhat, apothem, mainnet], 'name')
+export const networksById = keyBy([hardhat, apothem], 'id')
+export const networksByName = keyBy([hardhat, apothem], 'name')
+
 
 export const dappName = 'Oasis'
 export const pollingInterval = 12000
